07a695916ab8ed9266cad6fcaefd925c8ebde1d7
Isaak <krutisaak099@gmail.com>
2020-11-10 18:44:32 -0500

    Added Brew Beer Listener. Closes #8

---
+++ src/main/java/guru/springframework/msscbeerservice/services/brewing/BrewBeerListener.java
@@ -0,0 +1,36 @@
+package guru.springframework.msscbeerservice.services.brewing;
+
+import guru.springframework.msscbeerservice.config.JmsConfig;
+import guru.springframework.msscbeerservice.domain.Beer;
+import guru.springframework.msscbeerservice.events.BrewBeerEvent;
+import guru.springframework.msscbeerservice.events.NewBeerInventoryEvent;
+import guru.springframework.msscbeerservice.repositories.BeerRepository;
+import guru.springframework.msscbeerservice.web.model.BeerDto;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.jms.core.JmsTemplate;
+import org.springframework.stereotype.Service;
+import org.springframework.jms.annotation.JmsListener;
+
+@Slf4j
+@Service
+@RequiredArgsConstructor
+public class BrewBeerListener {
+
+    private final BeerRepository beerRepository;
+    private final JmsTemplate jmsTemplate;
+
+    @JmsListener(destination = JmsConfig.BREWING_REQUEST_QUEUE)
+    public void listen(BrewBeerEvent event){
+        BeerDto beerDto = event.getBeerDto();
+
+        Beer beer = beerRepository.getOne(beerDto.getId());
+
+        beerDto.setQuantityOnHand(beer.getQuantityToBrew());
+
+        NewBeerInventoryEvent newBeerInventoryEvent = new NewBeerInventoryEvent(beerDto);
+        jmsTemplate.convertAndSend(JmsConfig.NEW_INVENTORY_QUEUE, newBeerInventoryEvent);
+
+        log.debug("Brewed beer " + beer.getMinOnHand() + ": QOJ" + beerDto.getQuantityOnHand());
+    }
+}
